614773f418c2814d230ccece9a5dc3f6dd8564eb
Lehendss <edisonfcm1@gmail.com>
2022-11-30 22:07:17 +0000

    Added toDoList

--- projeto/main.c
+++ projeto/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+char menu();
 
 int main()
 {
-    printf("Hello world!\n");
+
+    menu();
+
+
     return 0;
+}
+
+
+char menu()
+{
+    char opcao;
+
+    printf("\n\t\tGESTAO DA REQUISACAO DE COMPUTADORES\n");
+    printf("\t\t____________________________________");
+    printf("\n\n\n»»Quantidade de portateis existentes\n ");
+    printf("\n»»Quantidade total de requisicoes efetuadas \n ");
+    printf("\n»»Quantidade de requisicoes ativas\n ");
+
+
+
+    scanf(" %c", opcao);
+    opcao = toupper(opcao);
+
+    return opcao;
 }
---
+++ projeto/toDoList.c
@@ -0,0 +1,102 @@
+void toDolist()
+{
+/*
+    #OBJETIVO#
+        +Pretende- se elaborar um programa em linguagem c que auxilie na gestao da requisicao de computadores
+    portateis pelos membros da comunidade  academica do Politecnico
+
+
+    #DESCRICAO DO PROBLEMA#
+        +Registrar informacao referente aos computadores
+            »As requisicoes efetuadas
+            »As respetivas devoluciones
+            »Averias
+            »Reparacoes ocorridas
+        +No registo de requisicao de um portatil
+            »Devera ser identificado o computador (disponivel) seleccionado pelo utente
+            »Prazo de requisicao (maximo 30 dias)
+            »Pode ser efetuada renovacoes por 7 dias
+            »Multa de 10 £ por cada dia de atraso
+        +Caso ocorra uma avaria
+            »Debe ser devolvido
+            »Apos  o registo de reparacao de um portail (avariado) ele volta a estar disponivel para ser requisitado
+
+    #DESCRICAO DOS ELEMENTOS#
+
+        +PORTATIL
+            »Caracterizado por
+                -Numero de identificacao (unico)
+                -Pela designacao
+                -Pelo procesador (i3, i5, i7)
+                -Pela RAM
+                -Pelo estado (disponivel, requisitado, avariado)
+                -Localizacao (Residencias, Campus1, Campus2, Campus5)
+                -Pela data de adquisicao
+                -Pelo valor do equipamento
+                -Existe MAXIMO 30 computadore portateis
+
+        +REQUISICAO
+            »Caracterizado por
+                -Codigo(String unico)
+                -Pelo numero de identificacao do portatil
+                -Pelo nome do utente
+                -Pelo tipo do utente(docente, estudante, tecnico administrativo)
+                -Pela data de requisicao (ativa, concluida)
+            »Registrar data de devolucao
+                -local de devolucao(Residencias, Campus1, Campus2, Campus5)
+                -Valor da multa paga
+
+x
+
+
+    ##CREEAR UN MENU PRINCIPAL:
+    Con las siguientes opciones
+
+        #Quantidade de portateis existentes#
+        #Quantidade de portateis disponiveis#
+        #Quantidade total de requisicoes efetuadas#
+        #Quantidade de requisicoes ativas#
+
+    ##FUNCIONES##
+
+    #Inserir# informacao referente a todos os portateis
+
+    #listar# informacao referente a todos os portateis
+        +Listar para cada portatil:
+            »Quatidade de avarias
+            »Quantidade de requisicoes
+            »Informacao
+                -Tipo de utente
+                -Prazo de requisicao
+
+    #Registar# a alteracao  da localidade de um computador
+
+    #Listar# os dados de todas as  requisicoes
+        +Para cada requisicao concluida deverao mostrar
+            »Duracao em dias
+            »O campues onde foi devolvido
+            »O valor da multa paga
+
+    #Registar# a avaria/ reparacao de um computador
+
+    #Armazenar e Ler# em ficheiros binarios
+        + A informacao  referente aos computadores
+        + Requisicoes efetuadas
+
+    #Apresentar# Datos estadisticos
+        +Percentagem de portateis com cada tipo de processador
+        +Custo medio de cada multa paga
+        +Tipo(s) de utentes com a menor quantidade de requisicoes efetuadas
+        +Devolucao(oes) mais recente(s)
+
+    #Apresentar # Uma listagem de todas a as avarias ocorridas contendo  a siguente informacao:
+        +Dados do portatil
+        +Tipo da avaria
+            »Temporaria
+            »Permanente
+        +Data de avaria(em dias)
+        +Tempo de duracao(em dias)
+
+
+*/
+}
